91d93e867b20c83e01bcca2752cd5eccd7afb262
Faraaz Nadeem <faraaz@dhcp-18-189-33-53.dyn.MIT.EDU>
2018-05-15 17:11:55 -0400

    Added graphviz blockchain visualization

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@ import pickle
 from datetime import datetime
 from datetime import timedelta
 import matplotlib.pyplot as plt
+import graphviz
 
 def mine_rate_info(endpoint_block, origin_block, block_information, time_interval):
 	endpoint_dt = datetime.fromtimestamp(highest_block[0]['timestamp'])
@@ -35,9 +36,23 @@ def aggregate_info(mined_buckets):
 			aggregate_buckets[i] = aggregate_buckets[i-1] + mined_buckets[i]
 	return aggregate_buckets
 
-# graph block mining time
+def generate_graphviz(block_information):
+	g = graphviz.Digraph('G', filename='block_information.gv')
+	g.node("origin", "")
+	for block_hash in block_information:
+		g.node(block_hash, "")
+		prev_hash = block_information[block_hash]['blockInformation']['previousHash']
+		if prev_hash == '':
+			prev_hash = "origin"
+		g.edge(prev_hash, block_hash)
+	g.view()
+
 block_information = pickle.load(open("block_information.pickle", 'rb'))
 highest_block = pickle.load(open("highest_block.pickle", 'rb'))
+
+print("Creating graphviz...")
+generate_graphviz(block_information)
+print("Done.")
 
 # block height 0: 6c179f21e6f62b629055d8ab40f454ed02e48b68563913473b857d3638e23b28
 origin_block = "6c179f21e6f62b629055d8ab40f454ed02e48b68563913473b857d3638e23b28"
@@ -48,7 +63,9 @@ time_interval = 60 # seconds
 times_list, mined_buckets = mine_rate_info(endpoint_block, origin_block, block_information, time_interval)
 aggregate_buckets = aggregate_info(mined_buckets)
 
+print("Plotting data...")
 line1, = plt.plot(times_list, mined_buckets, label="blocks mined / {}s".format(time_interval))
 line2, = plt.plot(times_list, aggregate_buckets, label="total blocks mined")
 plt.legend(handles=[line1, line2])
 plt.show()
+print("Done")
